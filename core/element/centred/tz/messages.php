eb3b7adbea8a0f56f10955c1ff4ab1199648603e
charl-fgx <charl@fgx.co.za>
2018-05-01 02:41:27 +0200

    Added reply to memo messages

--- core/element/centred/tz/messages.php
+++ core/element/centred/tz/messages.php
@@ -9,29 +9,31 @@ class messages extends Element {
         $this->element = __class__;
         parent::__construct(__class__);
     }
-    
+
+    public $read = 0;
+
     public function render() {
         $messageObj = new \Message();
-        $resMessages = $messageObj->getMessages();
-        $messages = [];
-        if (!empty($resMessages)) {
-            foreach($resMessages as $message) {
-                if ($message->is_read != 1) {
-                    $messages[] = $message;
-                }
-            }
-            if (count($resMessages) > 0) {
-                $this->setData('messages',$resMessages);
-            } else {
-                $this->setData('messages',[0 => (object)[
-                    'title'      => 'Inbox Empty',
-                    'created_at' => date('Y-m-d H:i:s'),
-                    'message'    => 'You currently have no unread messages',
-                    'is_read'    => 0,
-                    'hideButton' => true
-                ]]);
-            }
-        }
+        $messages = $messageObj->getMessages($this->session('messageReadStatus'));
+        //$messages = [];
+        //if (!empty($resMessages)) {
+        //    foreach($resMessages as $message) {
+        //        if ($message->is_read !== 1) {
+        //            $messages[] = $message;
+        //        }
+        //    }
+        //    if (count($resMessages) > 0) {
+        //        $this->setData('messages',$resMessages);
+        //    } else {
+        //        $this->setData('messages',[0 => (object)[
+        //            'title'      => 'Inbox Empty',
+        //            'created_at' => date('Y-m-d H:i:s'),
+        //            'message'    => 'You currently have no unread messages',
+        //            'is_read'    => 0,
+        //            'hideButton' => true
+        //        ]]);
+        //    }
+        //}
         $this->setData('spacerHeight',306 - (count(@$messages)+1) * 110);
         return (new view('messages.phtml',__DIR__))->html;
     }
