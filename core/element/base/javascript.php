aac86157aaaeb16d683c05bc0057f94c24bfd60e
charl-fgx <charl@fgx.co.za>
2018-05-08 15:17:35 +0200

    Added the following new properties to the "javascript" element: async, defer, charset and lazyload

--- core/element/base/javascript.php
+++ core/element/base/javascript.php
@@ -5,16 +5,39 @@ use core\Element;
 class javascript extends Element {
     public $src;
     public $path;
+    public $async;
+    public $defer;
+    public $charset = 'UTF-8';
+    public $lazyload = false;
 
     private function jscript() {
         $this->setElement('script');
     }
-    
+
     public function render() {
+        // Just easier to remember
+        if ($this->lazyload) {
+            $this->defer = 'defer';
+        }
+
         if (strpos($this->src,'/app') === 0) {
             $this->src = str_replace(env('project.url'),env('core.url'),$this->src);
         }
         $this->jscript();
+
+        // basically if this attribute is set in any way it will be loaded in async
+        if (isset($this->async)) {
+            $this->addAttr('async','async');
+        }
+
+        // basically if this attribute is set in any way it will be (lazyloaded)
+        if (isset($this->defer)) {
+            $this->addAttr('defer','defer');
+        }
+
+        if (isset($this->charset)) {
+            $this->addAttr('charset',$this->charset);
+        }
         return parent::render();
     }
 }
