3aecd5606d443c336f810991f0d9b44fee2ee93f
charl-fgx <charl@fgx.co.za>
2018-05-08 15:23:43 +0200

    Added the following methods: forgetInput, forgetEmptyInput

--- core/system/traits/core/InputTraits.php
+++ core/system/traits/core/InputTraits.php
@@ -6,13 +6,56 @@ trait InputTraits  {
         if (is_array($inputKey) && count($inputKey) == 1) {
            $inputKey = (string)$inputKey[0];
         }
-        if (isset($_GET[$inputKey])) {
+        if (isset($_GET[$inputKey]) && !empty($_GET[$inputKey])) {
             return true;
         }
-        if (isset($_POST[$inputKey])) {
+        if (isset($_POST[$inputKey]) && !empty($_POST[$inputKey])) {
             return true;
         }
         return false;
+    }
+
+    /* Input keys will get unset in the order specified in the $methodsToForget array.
+    If a string is specified with just once method only the once method's key will be unset.
+    If an empty string or array is sent for the $methodsToForget all the methods will be unset
+    starting with GET, POST. The last Method unset' old value will be returned */
+
+    /* $methodsToForget can be an array, string or null. */
+    public function forgetInput($inputKey,$methodsToForget = ['get','post']) {
+        $oldValue = null;
+        if (is_string($methodsToForget)) {
+            if (strtolower($methodsToForget) === 'get' && isset($_GET[$inputKey])) {
+                $oldValue = $_GET[$inputKey];
+                unset($_GET[$inputKey]);
+            }
+        } elseif (is_array($methodsToForget) && count($methodsToForget) >= 1) {
+            foreach($methodsToForget as $method) {
+                if (strtolower($method) === 'get' && isset($_GET[$inputKey])) {
+                    $oldValue = $_GET[$inputKey];
+                    unset($_GET[$inputKey]);
+                }
+                if (strtolower($method) === 'post' && isset($_POST[$inputKey])) {
+                    $oldValue = $_POST[$inputKey];
+                    unset($_POST[$inputKey]);
+                }
+            }
+        } elseif((is_array($methodsToForget) && count($methodsToForget)) == 0 || empty($methodsToForget)) {
+            $this->forgetInput($inputKey,['get','post']);
+        }
+        return $oldValue;
+    }
+
+    public function forgetEmptyInput() {
+        foreach($this->input() as $inputKey => $inputValue) {
+            if (empty($inputValue)) {
+                if (isset($_GET[$inputKey])) {
+                    unset($_GET[$inputKey]);
+                }
+                if (isset($_POST[$inputKey])) {
+                    unset($_POST[$inputKey]);
+                }
+            }
+        }
     }
 
     public function input($inputKey = null,$default = null) {
