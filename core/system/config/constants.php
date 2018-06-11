d7902b8136d9c04d2b970da4dc8243b7c76ce7a8
charl-fgx <charl@fgx.co.za>
2018-06-11 15:26:46 +0200

    Added some more date and time constants to constants.php

--- core/system/config/constants.php
+++ core/system/config/constants.php
@@ -12,10 +12,10 @@ if (!defined(PHP_VERSION)) {
 }
 
 // Check SSL certificate | $_SERVER['HTTPS'] = 'off' on IIS
-if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') { 
+if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
     define('SSL',true);
     define('HTTP','https');
-} else { 
+} else {
     define('SSL',false);
     define('HTTP','http');
 }
@@ -27,16 +27,21 @@ foreach((array)env('projects') as $project => $settings) {
 }
 define('PROJECT_LIST',serialize($projectList));
 
-// Date
+// Dates Current
 define('NOW',date ('Y-m-d H:i:s'));
 define('CURRENT_DATE',date ('Y-m-d'));
-define('CURRENT_YEAR',date ('Y-m-d'));
-define('CURRENT_YEAR_ONLY',date('Y'));
-define('PREVIOUS_YEAR',date ('Y-m-d', strtotime('-1 years')));
-define('NEXT_YEAR',date ('Y-m-d', strtotime('+1 years')));
-define('CURRENT_MONTH',date ('Y-m-d'));
-define('PREVIOUS_MONTH',date ('Y-m-d',strtotime('-1 months')));
-define('NEXT_MONTH',date ('Y-m-d', strtotime('+1 months')));
 define('CURRENT_TIME',date('H:i:s'));
 define('CURRENT_TIMESTAMP',date('Y-m-d H:i:s'));
 define('CURRENT_UNIX_TIMESTAMP',strtotime(date('Y-m-d H:i:s')));
+// Date Years
+define('CURRENT_YEAR_ONLY',date('Y'));
+define('CURRENT_YEAR',date ('Y-m-d'));
+define('PREV_YEAR',date ('Y-m-d', strtotime('-1 years')));
+define('NEXT_YEAR',date ('Y-m-d', strtotime('+1 years')));
+// Date Weeks
+define('PREV_WEEK',date ('Y-m-d',strtotime('-1 weeks')));
+define('NEXT_WEEK',date ('Y-m-d',strtotime('+1 weeks')));
+// Date Months
+define('CURRENT_MONTH',date ('Y-m-d'));
+define('PREV_MONTH',date ('Y-m-d',strtotime('-1 months')));
+define('NEXT_MONTH',date ('Y-m-d',strtotime('+1 months')));
