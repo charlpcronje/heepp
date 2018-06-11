3acd4689ef104f958f316e38e2cced14c45a14bc
charl-fgx <charl@fgx.co.za>
2018-06-11 15:07:23 +0200

    Added colorDarken helper function to darken HEX color codes

---
+++ core/system/helpers/css.php
@@ -0,0 +1,17 @@
+<?php
+
+function colorDarken($rgb,$darker=2) {
+    if (strpos($darker,'%') !== false) {
+        $darker = (int)str_replace('%','',$darker);
+        $darker = round($darker / 25,2);
+    }
+    $hash = (strpos($rgb, '#') !== false) ? '#' : '';
+    $rgb = (strlen($rgb) == 7) ? str_replace('#', '', $rgb) : ((strlen($rgb) == 6) ? $rgb : false);
+    if(strlen($rgb) != 6) return $hash.'000000';
+    $darker = ($darker > 1) ? $darker : 1;
+    list($R16,$G16,$B16) = str_split($rgb,2);
+    $R = sprintf("%02X", floor(hexdec($R16)/$darker));
+    $G = sprintf("%02X", floor(hexdec($G16)/$darker));
+    $B = sprintf("%02X", floor(hexdec($B16)/$darker));
+    return $hash.$R.$G.$B;
+}
