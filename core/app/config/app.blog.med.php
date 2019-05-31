ce18f6cbe58db97c16bb5ba1ffff77f3f108b58b
Charl Cronje <charlcp@gmail.com>
2019-05-31 14:50:01 +0200

    Update app.blog.med.php

--- core/app/config/app.blog.med.php
+++ core/app/config/app.blog.med.php
@@ -5,7 +5,7 @@ return [
     'get'          => '\\core\\extension\\api\\ReqGet',
     'post'         => '\\core\\extension\\api\\ReqPost',
     'verb'         => 'GET',
-    'url'          => 'https://cmscentred.co.za/api/v1/',
+    'url'          => 'http://localhost/api/v1/',
     'token'        => '2d008a9b6f32dd46f3b81e2ada86598210f59054697eb7a420ddc1f98f8afa151',
     'log.requests' => true
 ];
