219fc4f10991cbdb85a0b65725d5f3e96d9a6af4
Charl Cronje <charlcp@gmail.com>
2019-05-31 14:49:28 +0200

    Update app.api.cms.php

--- core/app/config/app.api.cms.php
+++ core/app/config/app.api.cms.php
@@ -4,8 +4,7 @@ return [
     |---------------------- */
     'class'        => '\\core\\extension\\centred\\CMSReq',
     'verb'         => 'GET',
-    //'url'           => 'http://localhost:8000/api/v1/',
-    'url'          => 'https://cmscentred.co.za/api/v1/',
-    'auth.key'     => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjM1ZTE4YzE0OTU2ZjFlNmZiZTBlNmNkOGFlMzk1MDU1MmQyYjIyMGU0ZTk0OWY3OGFmZDM0OWI5OTBjMmNiNmY2ZDMxODdlZmIzMjA1ZDNmIn0.eyJhdWQiOiIxIiwianRpIjoiMzVlMThjMTQ5NTZmMWU2ZmJlMGU2Y2Q4YWUzOTUwNTUyZDJiMjIwZTRlOTQ5Zjc4YWZkMzQ5Yjk5MGMyY2I2ZjZkMzE4N2VmYjMyMDVkM2YiLCJpYXQiOjE1MTcyMDIyOTIsIm5iZiI6MTUxNzIwMjI5MiwiZXhwIjoxNTQ4NzM4MjkyLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.nmo2xs2qiGroeFKrzCLI1gg3Pe37dBzQewWpPqvlzAoYcNYEiSzSCy23qAYpP7bXKVaOatMypKcy6ERwSjicMweJ9bbGOqEKuoY38UDJxAa7MPoq5M_yvBFso25mabytXemtog50y4cBaNk5sdUVkQ7oAgNDt87Lx-Z9mXVe5SoVPa2lD-xY4e2DLd82aO_1SnwLCKD5xeSOclnYhyyfuHWCu8iIIIsWMC8bdP1Hfl6pOhiO7JHjBzyA0fmfhvmncOo6bDiRyLto5U8ntJFCHNQ4lCKS4mz90gkH0fVAEWPMdrDrBFuBt5IQRpG71wJZ6PjCKxLo1oEG4pkhbkf7EPi25CXfsBhAAwpl78RqC0wXcHR67DYVBUKpT2gMvjcq18M1SsateqBDwxVG0tbocB_f9lWamW2z4QIhFozRrFVl8_EoAiqQS5-uRJ_bLSg_2ra6wbne3IkqyOEhHrMipCUHwS4SyriRYHe-b12x3qAJV5Ta3lsNFdCjH9lng4fp89bcm92vbGH5Y3N8U8nuwsDByvvkPDDWq2l16izvyg3JFqOuFWTFeH7ly4yhoMl0oL5V7IxPY-xhr8NikidyVJQ3uIMy_RO4txuYCfUAhhCdObIQxB4WwzboJOH445PMeCjZiPKETb5CA95uo3flbMaXQ_q9ss3gPDYTELXI5PU',
+    'url'           => 'http://localhost:8000/api/v1/',
+    'auth.key'     => '',
     'log.requests' => true
 ];
