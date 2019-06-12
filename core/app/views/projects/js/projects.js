1e85446b06adff5866d87b18f4c9f71e13a0b0fc
Charl Cronje <charlcp@gmail.com>
2019-06-12 15:05:27 +0200

    Change some Views

--- core/app/views/projects/js/projects.js
+++ core/app/views/projects/js/projects.js
@@ -3,6 +3,13 @@
 class projects {
     constructor() {
         this.project = {};
+        this.ws      = $('#cc-workspace');
+        this.wsLeft  = this.ws.find('section.ws-left');
+        this.wsRight = this.ws.find('section.ws-right');
+    }
+
+    getCurrentProject() {
+        return 'test';
     }
 
     init() {
