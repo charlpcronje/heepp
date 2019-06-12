605ce2b9002b4406c3c1273eed4f9e42b9e65a79
Charl Cronje <charlcp@gmail.com>
2019-06-12 14:56:21 +0200

    Change some variable names

--- core/app/assets/js/shortcuts.js
+++ core/app/assets/js/shortcuts.js
@@ -2,20 +2,20 @@
 
 class shortcuts {
     constructor() {
-        this.fgx = false;
+        this.heepp = false;
     }
 
     init() {
         var self = this;
         Mousetrap.bind('shift+f shift+g shift+x', function(e) {
-            self.fgx = true;
+            self.heepp = true;
             $.ajax({
                 url : UI_CONSTANTS.PROJECT.BASE_PATH+'core@console'
             });
         });
 
         Mousetrap.bind('`',function(e) {
-            if (self.fgx) {
+            if (self.heepp) {
                 $.ajax({
                     url : UI_CONSTANTS.PROJECT.BASE_PATH + 'core@console'
                 });
