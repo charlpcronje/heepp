b27590ad69aac3c528fbbeb8f1109a1e9e975c31
Charl Cronje <charl@gamezboost.com>
2019-07-08 11:42:01 +0200

    Delete Core App

--- core/app/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Specify HeEPP Path Below */
-$basePath = realpath('../../');
-chdir($basePath.DIRECTORY_SEPARATOR);
-
-/* Include HeEPP index.php file */
-include realpath($basePath).DIRECTORY_SEPARATOR.'index.php';;
